1995b9de1b0c321ce04ce6e24221df242ac9c935
João Renato <=>
2019-06-13 11:33:55 -0300

    Base do projeto

---
+++ src/test/java/br/com/esig/contato/ContatoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.esig.contato;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContatoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
